2a1b9c3210ad83969dec045ec60777abd860efd7
Lidiane Chen <62903825+LidianeChen@users.noreply.github.com>
2020-07-24 14:57:52 -0300

    Add files via upload

---
+++ Desmorgue/Desmorgue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Desmorgue
+//
+//  Created by Lidiane Chen on 24/07/20.
+//  Copyright © 2020 Lidiane Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Desmorgue/DesmorgueTests/DesmorgueTests.swift
@@ -0,0 +1,34 @@
+//
+//  DesmorgueTests.swift
+//  DesmorgueTests
+//
+//  Created by Lidiane Chen on 24/07/20.
+//  Copyright © 2020 Lidiane Chen. All rights reserved.
+//
+
+import XCTest
+@testable import Desmorgue
+
+class DesmorgueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
