6a8bfca59e5936dc7cd3940e6fc6132602310232
Marcos Vinicius <marcosviniciusmda@gmail.com>
2020-07-27 16:36:56 -0300

    Extensions

---
+++ Desmorgue/Desmorgue/Extensions.swift
@@ -0,0 +1,49 @@
+//
+//  Extensions.swift
+//  Desmorgue
+//
+//  Created by Marcos Vinicius Majeveski De Angeli on 27/07/20.
+//  Copyright © 2020 Lidiane Chen. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
---
+++ Desmorgue/Desmorgue/TabBar.swift
@@ -0,0 +1,24 @@
+//
+//  TabBar.swift
+//  Desmorgue
+//
+//  Created by Marcos Vinicius Majeveski De Angeli on 27/07/20.
+//  Copyright © 2020 Lidiane Chen. All rights reserved.
+//
+
+
+import UIKit
+import SwiftUI
+
+class TabController:UITabBar, UITabBarDelegate{
+   
+        
+    //Definindo Tela e ícone 1
+    let home = ContentView.self
+    let icon1 = UITabBarItem(title: "Início", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+    hom
+        
+        
+
+}
+
