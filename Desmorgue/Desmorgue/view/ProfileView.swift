c65a801785ca0b822a5d1a9adce76f4ab53f4769
Marcos Vinicius <marcosviniciusmda@gmail.com>
2020-08-04 16:54:24 -0300

    POR FAVOR

---
+++ Desmorgue/Desmorgue/view/ProfileView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileView.swift
+//  Desmorgue
+//
+//  Created by Marcos Vinicius Majeveski De Angeli on 04/08/20.
+//  Copyright © 2020 Lidiane Chen. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProfileView_Previews:PreviewProvider{
+    static var previews: some View{
+        ProfileView()
+    }
+}
+
+
+
+struct ProfileView:View {
+    var body: some View{
+        VStack(){
+            Text("Esse é o perfil")
+        }
+    }
+}
